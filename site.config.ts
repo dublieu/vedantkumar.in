e6690875c1764cf1ece2f87350fa3b149111e074
Dublieu Educational Services LLP <131824136+dublieu@users.noreply.github.com>
2025-01-13 23:27:55 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '17a24011ed0e80358bfcc191de09daea',
+  rootNotionPageId: '17a24011-ed0e-8035-8bfc-c191de09daea',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
